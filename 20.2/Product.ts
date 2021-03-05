cc930c9a4d847c4997f9d1e3b86005e23eed8654
Stefan Teneff <st.teneff@gmail.com>
2021-03-05 16:34:34 +0200

    Adds documents changes for OCAPI v19.10

--- 20.2/Product.ts
+++ 20.2/Product.ts
@@ -1,161 +1,40 @@
-import { Localized } from "./Localized";
-import { ProductLink } from "./ProductLink";
-import { ProductPromotion } from "./ProductPromotion";
-import { ProductType } from "./ProductType";
-import { Recommendation } from "./Recommendation";
-import { SiteSpecific } from "./SiteSpecific";
-import { Variant } from "./Variant";
-import { VariationAttribute } from "./VariationAttribute";
-import { VariationGroup } from "./VariationGroup";
-import { Master } from "./Master";
-import { Option } from "./Option";
-import { Inventory } from "./Inventory";
-import { ImageGroup } from "./ImageGroup";
-import { BundledProduct } from "./BundledProduct";
+import * as Previous from "../19.10";
 
-export interface Product {
+type CatalogCategoryId = string | unknown;
+
+export interface Product extends Previous.Product {
   /**
-   * The product's brand.
+   * The classification category (default category) of the product
    */
-  brand: string;
+  classification_category: CatalogCategoryId;
+
   /**
-   * The array of all bundled products of this product.
+   *
    */
-  bundled_products: BundledProduct[];
+  localized_tax_class_id: string[];
+
   /**
-   * The ISO 4217 mnemonic code of the currency.
+   * The price per unit of the product
    */
-  currency: string;
+  price_per_unit: number;
+
   /**
-   * The European Article Number.
+   * The catalog categories that are primary for the product
    */
-  ean: string;
+  primary_categories: CatalogCategoryId[];
+
   /**
-   * mandatory=true, maxLength=100, minLength=1, nullable=false	The id (SKU) of the product.
+   * The catalog categories that the product is assigned to
    */
-  id: string;
-  /**
-   * The array of product image groups.
-   */
-  image_groups: ImageGroup[];
-  /**
-   * The array of product inventories explicitly requested via 'inventory_ids' query parameter. This property is only returned in context of the 'availability' expansion.
-   */
-  inventories: Inventory[];
-  /**
-   * The site default inventory information. This property is only returned in context of the 'availability' expansion.
-   */
-  inventory: Inventory;
-  /**
-   * The localized product long description.
-   */
-  long_description: Localized<string>;
-  /**
-   * The products manufacturer name.
-   */
-  manufacturer_name: string;
-  /**
-   * The products manufacturer sku.
-   */
-  manufacturer_sku: string;
-  /**
-   * The master product information. Only for types master, variation group and variant.
-   */
-  master: Master;
-  /**
-   * The minimum order quantity for this product.
-   */
-  min_order_quantity: number;
-  /**
-   * The localized product name.
-   */
-  name: Localized<string>;
-  /**
-   * The array of product options. This array can be empty. Only for type option.
-   */
-  options: Option[];
-  /**
-   * The localized products page description.
-   */
-  page_description: Localized<string>;
-  /**
-   * The localized products page description.
-   */
-  page_keywords: Localized<string>;
-  /**
-   * The localized products page title.
-   */
-  page_title: Localized<string>;
-  /**
-   * The sales price of the product. In case of complex products like master or set this is the minimum price of related child products.
-   */
-  price: number;
-  /**
-   * The maximum sales of related child products in case of complex products like master or set.
-   */
-  price_max: number;
-  /**
-   * The prices map with price book ids and their values.
-   */
-  prices: Record<string, number>;
-  /**
-   * The id of the products primary category.
-   */
-  primary_category_id: string;
-  /**
-   * The array of source and target products links information.
-   */
-  product_links: ProductLink[];
-  /**
-   * The array of active customer product promotions for this product. This array can be empty. Coupon promotions are not returned in this array.
-   */
-  product_promotions: ProductPromotion[];
-  /**
-   * Returns a list of recommendations.
-   */
-  recommendations: Recommendation[];
-  /**
-   * The array of set products of this product.
-   */
-  set_products: Product[];
-  /**
-   * The localized product short description.
-   */
-  short_description: Localized<string>;
-  /**
-   * The steps in which the order amount of the product can be increased.
-   */
-  step_quantity: number;
-  /**
-   * The product type information. Can be one or multiple of the following values: item,master,variation_group,variant,bundle,set.
-   */
-  type: ProductType;
-  /**
-   * The sales unit of the product.
-   */
-  unit: string;
+  tax_class_id: string;
+
   /**
    * The time when product is valid from.
    */
-  valid_from: SiteSpecific<Date>;
+  valid_from: Previous.SiteSpecific<Date>;
+
   /**
    * The time when product is valid to.
    */
-  valid_to: SiteSpecific<Date>;
-  /**
-   * The array of actual variants. This array can be empty. Only for types master, variation group and variant.
-   */
-  variants: Variant[];
-  /**
-   * Sorted array of variation attributes information. This array can be empty. Only for types master, variation group and variant.
-   */
-  variation_attributes: VariationAttribute[];
-  /**
-   * The array of actual variation groups. This array can be empty. Only for types master, variation group and variant.
-   */
-  variation_groups: VariationGroup[];
-  /**
-   * The actual variation attribute id - value pairs. Only for type variant and variation group.
-   */
-  variation_values: Record<string, string>;
+  valid_to: Previous.SiteSpecific<Date>;
 }
